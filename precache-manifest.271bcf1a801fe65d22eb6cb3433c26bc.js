171787604820207e836eb40a005f4b43b553ab68
Eshu Thakur (Ujjwal Jadoun) <37092836+ZadeThakur@users.noreply.github.com>
2019-06-29 05:26:19 -0230

    Updates

---
+++ precache-manifest.271bcf1a801fe65d22eb6cb3433c26bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eac60b08b5822ffc8e5f7f0e090f7fe",
+    "url": "/react-recipe-diary/index.html"
+  },
+  {
+    "revision": "2633f14dda6f988a75d8",
+    "url": "/react-recipe-diary/static/css/main.e9513a4a.chunk.css"
+  },
+  {
+    "revision": "fc7d82a4275d2e52553d",
+    "url": "/react-recipe-diary/static/js/2.f0a6dae1.chunk.js"
+  },
+  {
+    "revision": "2633f14dda6f988a75d8",
+    "url": "/react-recipe-diary/static/js/main.d6ecf5dc.chunk.js"
+  },
+  {
+    "revision": "4900bf4672b9087e156c",
+    "url": "/react-recipe-diary/static/js/runtime~main.b65eea13.js"
+  }
+]);
